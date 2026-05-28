4ea46a9e77dd4443b5c922f53f9d06c0ea80093e
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2026-05-28 18:07:56 +0200

    Assert y ejemplos

---
+++ EJERCICIOS/05_assert_python.py
@@ -0,0 +1,17 @@
+def sumar(numero_1: int, numero_2: int) -> int:
+    '''
+    Sumar dos números enteros.
+
+    Parámetros:
+    - numero_1 : primer sumando
+    - numero_2 : segundo sumando
+
+    Return:
+    - Suma de los dos parámetros
+    '''
+    assert isinstance(numero_1, int), 'numero_1 no es entero'
+    assert numero_1>0, 'numero_1 debe ser positivo'
+    resultado = numero_1 + numero_2
+    return resultado
+
+sumar(5, 2)
