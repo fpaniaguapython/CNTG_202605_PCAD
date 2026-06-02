a4a3e36f212875eaac9c334d691e3147a3b46e52
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2026-06-02 20:29:46 +0200

    Update 32_estadistica_descriptiva.py

--- EJERCICIOS/32_estadistica_descriptiva.py
+++ EJERCICIOS/32_estadistica_descriptiva.py
@@ -1,6 +1,8 @@
+import statistics
 import sqlite3
 
 import pandas as pd
+import numpy as np
 
 DB_NAME='db_empleados_optimizado.sqlite3'
 conn = sqlite3.connect(database=DB_NAME)
@@ -14,5 +16,16 @@ serie_np = df['salario'].to_numpy()
 print(type(lista_python)) # list
 print(type(serie_pandas)) # pandas.Serie
 print(type(serie_np)) # numpy.ndarry
+
+# Media con Python
+print('Media:',statistics.mean(lista_python))
+# Mediana con Numpy
+print('Mediana:', np.median(serie_np))
+# Moda con Pandas
+print('Moda:', serie_pandas.mode())
+# Desviación estándar con Python
+print('Desviación estándar poblacional con Python:', statistics.pstdev(lista_python))
+print('Desviación estándar poblacional con NumPy:', np.std(serie_np))
+print('Desviación estándar poblacional con Pandas:', serie_pandas.std(ddof=0))
 
 conn.close()
