0ccaf79bbf2b70e526fb454dd7dce180036809f9
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2026-06-04 17:16:42 +0200

    Create 04_get_from_dataframe_ancho_a_largo_con_melt.py

---
+++ EJERCICIOS/ejemplo_melt/04_get_from_dataframe_ancho_a_largo_con_melt.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Leer CSV
+df = pd.read_csv("./datos/datos_ventas_ancho.csv")
+
+# Convertir a formato largo
+df_long = df.melt(
+    id_vars="mes",
+    var_name="comunidad",
+    value_name="ventas"
+)
+df_long.head()
+
+# Gráfico
+plt.figure(figsize=(14, 6))
+
+sns.barplot(
+    data=df_long,
+    x="mes",
+    y="ventas",
+    hue="comunidad"
+)
+
+plt.title("Ventas por comunidad y mes")
+plt.xlabel("Mes")
+plt.ylabel("Ventas")
+plt.xticks(rotation=45)
+plt.legend(title="Comunidad", bbox_to_anchor=(1.05, 1), loc="upper left")
+plt.tight_layout()
+
+plt.show()
+
